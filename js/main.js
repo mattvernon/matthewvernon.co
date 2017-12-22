bdaba331bcbf6c70b88a84f4da3e6ed0da01a432
Matthew Vernon <hello@matthewvernon.co>
2017-12-22 16:34:52 +1100

    added in progress bars and other stuff

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
-$(document).on('scroll', function() {
-  var pixelsFromTop = $(document).scrollTop();
+// $(document).on('scroll', function() {
+//   var pixelsFromTop = $(document).scrollTop();
+//
+//   var documentHeight = $(document).height()
+//   var windowHeight = $(window).height()
+//
+//   var difference = documentHeight - windowHeight
+//
+//   var percentage = 100 * pixelsFromTop / difference
+//
+//   $('.progress-bar').css('width', percentage + 'vw')
+//
+//
+// })
 
-  var documentHeight = $(document).height()
-  var windowHeight = $(window).height()
+// progress bars
 
-  var difference = documentHeight - windowHeight
+$(document).on("scroll", function () {
+  // total bar dist
+  var barTotal = ($(window).height() * 2) + ($(window).width() * 2)
 
-  var percentage = 100 * pixelsFromTop / difference
+  // scroll total
+  var scrollTotal = $(document).height() - $(window).height()
 
-  $('.progress-bar').css('width', percentage + 'vw')
+  // current scroll position
+  var pixels = $(document).scrollTop()
 
+  // percantage scrolled
+  var pc = pixels / scrollTotal
 
+  // bar across percentage of total
+  var barAcross = $(window).width() / barTotal
+
+  // bar down percentage of total
+  var barDown = $(window).height() / barTotal
+
+  var bar1 = pc / barAcross
+  var bar2 = (pc - barAcross) / barDown
+  var bar3 = (pc - barAcross - barDown) / barAcross
+  var bar4 = (pc - barAcross - barDown - barAcross) / barDown
+
+  $("div.bar-1").css("width",  100 * bar1 + "%")
+  $("div.bar-2").css("height", 100 * bar2 + "%")
+  $("div.bar-3").css("width",  100 * bar3 + "%")
+  $("div.bar-4").css("height", 100 * bar4 + "%")
+
+  console.log(bar1)
 })
 
 // open linkedin in
@@ -81,6 +115,3 @@ $(".backtotop").click(function() {
   $("html, body").animate({ scrollTop: 0 }, 1600, "swing");
   return false;
 });
-
-
-// fade in sections
